01eaeaf90a343c7c5d4a0ef995c4a33f5a1f62bb
Stephen Benjamin <stephen@bitbin.de>
2013-06-06 17:02:12 +0200

    Added promote functionality

--- src/spaceclone/modules/promote.py
+++ src/spaceclone/modules/promote.py
@@ -1,9 +1,53 @@
-__module_name__ = "promote"
-__module_desc__ = "Promotes one snapshot to another"
-__module_help__ = "Promote help"
+__module_name__ = "Promote"
+__module_desc__ = "Promotes a cloneset to the same version as it's origin"
 
-def run(args, logger):
-    pass
+import sys
+from optparse import OptionGroup
+from prettytable import PrettyTable
+from ..satellite import Satellite, Cloneset, Clone
 
-def help():
-    return __module_help__    
+def run(parser, rhn, logger):
+
+    parser.add_satellite_options()
+
+    group = OptionGroup(parser.parser, "Promote Options")
+    group.add_option("-c", "--cloneset", action="store", type="string", dest="cloneset", help="Cloneset")
+    parser.add_group(group)
+    
+    parser.set_required(["sat_server", "sat_username", "sat_password", "cloneset"])
+
+    (options, args) = parser.parse()
+
+    rhn = Satellite(options.sat_server, options.sat_username, options.sat_password)
+
+    cloneset = rhn.cloneset_info(options.cloneset)
+
+    print "\nThe following target channels will be updated with the content in the origin channel:\n"
+
+    table = PrettyTable(["Origin", "", "Target"]) 
+
+    table.align = "l"
+    
+    channels = {}
+
+    for clone in [cloneset.base] + cloneset.children:
+        table.add_row([clone.source, "-->",  clone.label])
+        channels[clone.label] = clone.source
+
+    print table
+
+    yesno = raw_input("\nConfirm? [Y/n] ")
+
+    if yesno.rstrip() != "Y":
+        print "Aborted."
+        sys.exit()
+    
+    for target, origin in channels.iteritems():
+        sys.stdout.write("Merging %s to %s... " % (origin, target))
+        sys.stdout.flush()
+        rhn.sat.channel.software.mergePackages(rhn.key, origin, target)
+        sys.stdout.write("[ OK ]\n")
+        sys.stdout.flush()
+
+    print ""
+

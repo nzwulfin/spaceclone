08c2b9f8fef113a20e7e4101455399727ed0ff7a
Stephen Benjamin <stephen@bitbin.de>
2013-06-18 22:33:11 +0200

    Prompt for options interactively if user doesn't provide them (hacky for now... )

--- src/spaceclone/options.py
+++ src/spaceclone/options.py
@@ -1,45 +1,66 @@
 import sys
+from getpass import getpass
 from optparse import OptionParser, OptionGroup
 
 class Parser:
 
-        def __init__(self, args):
-            usage = "usage: %prog [options]"
-	    if len(args) > 1:
-		usage = "usage: %prog " + args[1] + " [options]"
+    def __init__(self, args):
+        usage = "usage: %prog [options]"
+        if len(args) > 1:
+            usage = "usage: %prog " + args[1] + " [options]"
 
-            self.args = args
-            self.parser = OptionParser(usage=usage)
-            self.required = None
+        self.args = args
+        self.parser = OptionParser(usage=usage)
+        self.required = None
 
-        def add_group(self, optgroup):
-            self.parser.add_option_group(optgroup)
+    def add_group(self, optgroup):
+        self.parser.add_option_group(optgroup)
 
-        def set_required(self, required):
-            self.required = required
+    def set_required(self, required):
+        self.required = required
 
-        def add_satellite_options(self):
-            satgroup = OptionGroup(self.parser, "Satellite Options")
+    def add_satellite_options(self):
+        satgroup = OptionGroup(self.parser, "Satellite Options")
             
-            satgroup.add_option("-s", "--server", action="store", type="string", dest="sat_server", help="Server Name")
-            satgroup.add_option("-u", "--username", action="store", type="string", dest="sat_username", help="Username")
-            satgroup.add_option("-p", "--password", action="store", type="string", dest="sat_password", help="Password")
+        satgroup.add_option("-s", "--server", action="store", type="string", dest="sat_server", help="Satellite Server Name")
+        satgroup.add_option("-u", "--username", action="store", type="string", dest="sat_username", help="Satellite Username")
+        satgroup.add_option("-p", "--password", action="store", type="string", dest="sat_password", help="Satellite Password")
 
-            self.parser.add_option_group(satgroup)
+        self.parser.add_option_group(satgroup)
 
-        def parse(self):
-            (options, args) = self.parser.parse_args(self.args)
-            if self.required:
-                missing = []
-                for requirement in self.required:
-                    if not options.__dict__[requirement]:
-                        missing.append(requirement)
-                if missing != []:
-                    self.parser.print_help()
-                    print ""
-                    print "** Missing Required Options: " + str(missing)
-                    sys.exit()
+    def parse(self):
+        (options, args) = self.parser.parse_args(self.args)
 
-            return (options, args)
-                 
+        # This is really hacky, is there a better way?
+        # Prompt users to provide required options interactively
+        # if they're missing
 
+        if self.required:
+            missing = []
+            for requirement in self.required:
+                if not options.__dict__[requirement]:
+                    missing.append(requirement)
+            if missing != []:
+                for destination in missing:
+                    option = self.get_option(destination)
+                    if "password" not in option.help.lower():
+                        answer = raw_input(option.help + ": ")
+                    else:
+                        answer = getpass(option.help + ": ")
+
+                    if answer == None:
+                        self.parser.print_help()
+                        print "Invalid Option: " + option.help
+                        sys.exit()
+                    else:
+                        self.args.append(option._short_opts[0])
+                        self.args.append(answer)
+
+        (options, args) = self.parser.parse_args(self.args)
+        return (options, args)
+
+    def get_option(self, destination):
+        for group in self.parser.option_groups:
+            for option in group.option_list:
+                if destination == option.dest:
+                    return option
